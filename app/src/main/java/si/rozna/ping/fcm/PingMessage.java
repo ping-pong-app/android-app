0c19fcd3e5e466f2193f6748d2eb9676700790b1
Jamsek-m <miha_jamsek@windowslive.com>
2020-08-14 12:55:35 +0200

    added fcm logic (start)

--- app/src/main/java/si/rozna/ping/fcm/PingMessage.java
+++ app/src/main/java/si/rozna/ping/fcm/PingMessage.java
@@ -6,12 +6,26 @@ import java.util.Map;
 
 public class PingMessage {
 
+    /**
+     * ID of ping entity
+     */
     private String id;
 
+    /**
+     * User data of ping initiator
+     */
     private User user;
 
+    /**
+     * Group data of pinged group
+     */
     private Group group;
 
+    /**
+     * Constructs new message instance from FCM message
+     *
+     * @param message message received from FCM broker
+     */
     public PingMessage(RemoteMessage message) {
         Map<String, String> data = message.getData();
         this.user = new User(data.get("userId"), data.get("username"));
@@ -32,7 +46,15 @@ public class PingMessage {
     }
 
     public static class User {
+
+        /**
+         * User id
+         */
         private String id;
+
+        /**
+         * User username
+         */
         private String username;
 
         private User(String id, String username) {
@@ -50,7 +72,15 @@ public class PingMessage {
     }
 
     public static class Group {
+
+        /**
+         * Group id
+         */
         private String id;
+
+        /**
+         * Group name
+         */
         private String name;
 
         private Group(String id, String groupName) {
--- app/src/main/java/si/rozna/ping/fcm/PingMessageService.java
+++ app/src/main/java/si/rozna/ping/fcm/PingMessageService.java
@@ -1,16 +1,54 @@
 package si.rozna.ping.fcm;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import si.rozna.ping.R;
+
 public class PingMessageService extends FirebaseMessagingService {
+
+    public static final String CHANNEL_ID = "si.rozna.ping.ANDROID";
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         PingMessage message = new PingMessage(remoteMessage);
 
+        String notificationMessage = String.format("User %s pinged group %s!", message.getUser().getUsername(), message.getGroup().getName());
 
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("PING")
+                .setContentText(notificationMessage)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+
+
+
+    }
+
+    private void createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = getString(R.string.ping_channel);
+            String desc = getString(R.string.ping_channel_desc);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(desc);
+            channel.enableVibration(true);
+            channel.enableLights(true);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
